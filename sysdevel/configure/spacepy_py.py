9da5a0114960578d72ec66fd2721c3c16c82374d
Sean Brennan <brennan@lanl.gov>
2013-05-16 11:48:57 -0600

    Added spacepy config.

---
+++ sysdevel/configure/spacepy_py.py
@@ -0,0 +1,25 @@
+
+from sysdevel.util import *
+from sysdevel.configuration import py_config
+
+class configuration(py_config):
+    """
+    Find/install SpacePy
+    """
+    def __init__(self):
+        py_config.__init__(self, 'spacepy', '0.1.3',
+                           dependencies=['gfortran', 'cdf', 'ffnet'],
+                           debug=False)
+
+
+    def install(self, environ, version, locally=True):
+        if not self.found:
+            website = 'http://downloads.sourceforge.net/project/spacepy/' + \
+                'spacepy/spacepy-' + str(version) + '/'
+            if version is None:
+                version = self.version
+            src_dir = 'spacepy-' + str(version)
+            archive = src_dir + '.tar.gz' 
+            install_pypkg(src_dir, website, archive, locally=locally)
+            if not self.is_installed(environ, version):
+                raise Exception('SpacePy installation failed.')
