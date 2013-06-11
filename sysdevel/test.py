9a6b09051fa23844bb8558e8a7a4267efb4d03be
Sean Brennan <brennan@lanl.gov>
2013-06-11 14:31:55 -0600

    Added C & C++ unit testing.

--- sysdevel/test.py
+++ sysdevel/test.py
@@ -63,16 +63,31 @@ class test(Command):
         return len(self._get_fortran_tests()) > 0
 
 
-    def _get_cpp_tests(self):
+    def _get_c_tests(self):
         ctests = []
+        if self.tests:
+            for pkg, tests in self.tests:
+                for unit in units:
+                    if isinstance(unit, CUnitTest):
+                        pkgtests.append(unit)
+                if len(pkgtests) > 0:
+                    ctests.append((pkg, pkgtests))
+        return ctests
+
+    def _has_c_tests(self):
+        return len(self._get_c_tests()) > 0
+
+
+    def _get_cpp_tests(self):
+        cpptests = []
         if self.tests:
             for pkg, tests in self.tests:
                 for unit in units:
                     if isinstance(unit, CppUnitTest):
                         pkgtests.append(unit)
                 if len(pkgtests) > 0:
-                    ctests.append((pkg, pkgtests))
-        return ctests
+                    cpptests.append((pkg, pkgtests))
+        return cpptests
 
     def _has_cpp_tests(self):
         return len(self._get_cpp_tests()) > 0
@@ -88,6 +103,7 @@ class test(Command):
                                 build.build_base, self.distribution.subpackages,
                                 argv, False)
 
+        ## PYTHON
         if self._has_python_tests():
             self.run_command('build')
             build = self.get_finalized_command('build')
@@ -127,6 +143,7 @@ class test(Command):
                 log.info('Python unit tests for ' + pkg)
                 util.check_call([wrap])
 
+        ## FORTRAN
         if self._has_fortran_tests():
             from configure import fruit
             env = dict()
@@ -142,7 +159,8 @@ class test(Command):
             lib_dir = build.build_temp
             for pkg, units in self._get_fortran_tests():
                 for unit in units:
-                    unit.sources += fortran_unittest_framework
+                    unit.sources = fortran_unittest_framework + unit.sources + \
+                        util.create_fruit_driver(unit.name, unit.sources[0])
                     unit.library_dirs.append(lib_dir)
                     self.distribution.native_executables.append(unit)
 
@@ -157,8 +175,42 @@ class test(Command):
                 for unit in units:
                     util.check_call([os.path.join(lib_dir, unit.name)])
 
+        ## C
+        if self._has_c_tests():
+            raise Exception("C unit testing not yet supported.") #FIXME
+
+            from configure import cunit
+            env = dict()
+            if not cunit.is_installed(env, None):
+                cunit.install(env, None)
+
+            orig_exes = self.distribution.native_executables
+
+            build = self.get_finalized_command('build')
+            lib_dir = build.build_temp
+            for pkg, units in self._get_c_tests():
+                for unit in units:
+                    unit.sources += util.create_cunit_driver(unit.name,
+                                                             unit.sources[0])
+                    unit.include_dirs.append(env['CUNIT_INCLUDE_DIR'])
+                    unit.libraries.append(env['CUNIT_LIBRARIES'])
+                    unit.library_dirs.append(env['CUNIT_LIB_DIR'])
+                    self.distribution.native_executables.append(unit)
+
+            ## build w/ distutils thru backdoor
+            cmd_obj = self.get_command_obj('build_exe')
+            cmd_obj.ensure_finalized()
+            cmd_obj.run()
+            self.distribution.native_executables = orig_exes
+
+            for pkg, units in self._get_c_tests():
+                log.info('C unit tests for ' + pkg)
+                for unit in units:
+                    util.check_call([os.path.join(lib_dir, unit.name)])
+
+        ## C++
         if self._has_cpp_tests():
-            raise Exception("C/C++ unit testing not yet supported.") #FIXME
+            raise Exception("C++ unit testing not yet supported.") #FIXME
 
             from configure import cppunit
             env = dict()
@@ -169,9 +221,9 @@ class test(Command):
 
             build = self.get_finalized_command('build')
             lib_dir = build.build_temp
-            for pkg, units in self._get_fortran_tests():
+            for pkg, units in self._get_cpp_tests():
                 for unit in units:
-                    unit.sources += util.create_cppunit_driver()
+                    unit.sources += util.create_cppunit_driver(unit.name)
                     unit.include_dirs.append(env['CPPUNIT_INCLUDE_DIR'])
                     unit.libraries.append(env['CPPUNIT_LIBRARIES'])
                     unit.library_dirs.append(env['CPPUNIT_LIB_DIR'])
@@ -183,7 +235,7 @@ class test(Command):
             cmd_obj.run()
             self.distribution.native_executables = orig_exes
 
-            for pkg, units in self._get_fortran_tests():
-                log.info('C/C++ unit tests for ' + pkg)
+            for pkg, units in self._get_cpp_tests():
+                log.info('C++ unit tests for ' + pkg)
                 for unit in units:
                     util.check_call([os.path.join(lib_dir, unit.name)])
--- sysdevel/util.py
+++ sysdevel/util.py
@@ -708,6 +708,95 @@ def create_testscript(tester, units, target, pkg_dirs):
     os.chmod(target, 0777)
 
 
+def create_fruit_driver(unit, srcfile):
+    test_functions = ''
+    i = open(srcfile, 'r')
+    for line in i:
+        delim = 'subroutine'
+        if delim in line:
+            test_functions += 'call ' + line[len(delim)+1:] + '\n'
+    i.close()
+    test_driver = unit + '_fruit_driver.f90'
+    o = open(test_driver, 'w')
+    o.write('program ' + unit + '_fruit_driver\n' +
+            'use fruit\n' +
+            'use ' + unit + '\n' +
+            'call init_fruit\n' +
+            test_functions +
+            'call fruit_summary\n' +
+            'end program ' + unit + '_fruit_driver\n'
+            )
+    o.close()
+    return test_driver
+
+
+def create_cunit_driver(unit, srcfile):
+    i = open(srcfile, 'r')
+    lines = i.readlines()
+    i.close()
+    setup_fctn = ''
+    if not 'setUp' in lines:
+        setup_fctn = 'int setUp(void) {\n  return 0;\n}\n'
+    teardown_fctn = ''
+    if not 'tearDown' in lines:
+        teardown_fctn = 'int tearDown(void) {\n  return 0;\n}\n'
+    tests = []
+    for line in lines:
+        start = line.find('void ')
+        end = line.find('(void)')
+        if start >= 0 and end > 0:
+            test_name = line[start+5:end-5]
+            tests.append('       (NULL == CU_add_test(pSuite, "' + test_name +
+                         '", ' + test_name + ')) ')
+    test_driver = unit + '_cunit_driver.cpp'
+    o = open(test_driver, 'w')
+    o.write('#include <CUnit/Basic.h>\n' +
+            setup_fctn + teardown_fctn + 
+            'int main(int argc, char* argv[]) {\n' +
+            '   CU_pSuite pSuite = NULL;\n' +
+            '   if (CUE_SUCCESS != CU_initialize_registry())\n' +
+            '      return CU_get_error();\n' +
+            '   pSuite = CU_add_suite("' + unit + '", setup, teardown);\n' +
+            '   if (NULL == pSuite) {\n' +
+            '      CU_cleanup_registry();\n' +
+            '      return CU_get_error();\n' +
+            '   }\n' +
+            '   if (\n' + 
+            '||\n'.join(tests) + '\n' +
+            '      ) {\n' +
+            '      CU_cleanup_registry();\n' +
+            '      return CU_get_error();\n' +
+            '   }\n' +
+            '   CU_basic_set_mode(CU_BRM_VERBOSE);\n' +
+            '   CU_basic_run_tests();\n' +
+            '   CU_cleanup_registry();\n' +
+            '   return CU_get_error();\n' +
+            '}\n' +
+            )
+    o.close()
+    return test_driver
+
+
+def create_cppunit_driver(unit):
+    test_driver = unit + '_cppunit_driver.cpp'
+    o = open(test_driver, 'w')
+    o.write('#include <cppunit/CompilerOutputter.h>\n' +
+            '#include <cppunit/extensions/TestFactoryRegistry.h>\n' +
+            '#include <cppunit/ui/text/TestRunner.h>\n' +
+            'int main(int argc, char* argv[]) {\n' +
+            '  CppUnit::Test *suite = CppUnit::TestFactoryRegistry::getRegistry().makeTest();\n' +
+
+            '  CppUnit::TextUi::TestRunner runner;\n' +
+            '  runner.addTest( suite );\n' +
+            '  runner.setOutputter( new CppUnit::CompilerOutputter( &runner.result(), std::cerr ) );\n' +
+            '  bool wasSucessful = runner.run();\n' +
+            '  return wasSucessful ? 0 : 1;\n' +
+            '}\n' +
+            )
+    o.close()
+    return test_driver
+
+
 def programfiles_directories():
     if 'windows' in platform.system().lower():
         try:
