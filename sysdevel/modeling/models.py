463b77c78b3455cba1a955e2a574fd519d6d464a
Sean Brennan <brennan@lanl.gov>
2014-05-19 16:23:51 -0600

    DataModel storage.

--- sysdevel/modeling/models.py
+++ sysdevel/modeling/models.py
@@ -27,8 +27,16 @@ permissions and limitations under the License.
 Model-View-Controller base classes
 """
 
+import os
+import shutil
 import math
+from itertools import combinations
+try:
+    import json
+except ImportError:
+    import simplejson as json
 
+from .websockethandler import json_handler
 
 class UnknownModelException(Exception):
     def __init__(self, explain):
@@ -37,6 +45,15 @@ class UnknownModelException(Exception):
 
     def __str__(self):
         return 'Unknown model ' + self.what
+
+
+class UnmatchedModelException(Exception):
+    def __init__(self, explain):
+        Exception.__init__(self)
+        self.what = explain
+
+    def __str__(self):
+        return 'Unmatched model ' + self.what
 
 
 
@@ -48,16 +65,23 @@ class DataModel(dict):
     multiple controllers, read by viewer for display.
     '''
 
-    date_format = '%Y-%m-%d'
-    time_format = '%H:%M:%S'
-    datetime_sep = 'T'
-    datetime_tz = 'Z'
-    datetime_format = date_format + datetime_sep + time_format + datetime_tz
+    DATE_FORMAT = '%Y-%m-%d'
+    TIME_FORMAT = '%H:%M:%S'
+    DATETIME_SEP = 'T'
+    DATETIME_TZ = 'Z'
+    DATETIME_FORMAT = DATE_FORMAT + DATETIME_SEP + TIME_FORMAT + DATETIME_TZ
 
-    def __init__(self):
+    def __init__(self, template=()):
         dict.__init__(self)
         self._pipeline = dict()
         self._index = 0
+        try:
+            d = dict(template)
+        except ValueError:
+            d = dict([(k, None) for k in template])
+        for k, v in d.items():
+            self[k] = v
+
 
     def __getattr__(self, attr):
         return self[attr]
@@ -91,6 +115,362 @@ class DataModel(dict):
             prev = ctrlr
         return True
 
+
+    @classmethod
+    def satisfies(cls, parameters, obj):
+        '''
+        Tests if values satisfy parameters.
+        obj is dictionary-like
+        '''
+        tests = [True] * len(parameters)
+        for i, (k, v) in enumerate(parameters.items()):
+            if obj[k] != v: #FIXME range testing?
+                tests[i] = False
+        return sum(tests)
+
+
+
+class CSVDataModel(DataModel):
+    '''
+    Comma-separated-value storage-backed data. Represents one row of a file.
+    '''
+    FIELD_SEP = ','
+
+    def __init__(self, template=()):
+        super(CSVDataModel, self).__init__(template)
+
+
+    def header(self):
+        return self.FIELD_SEP.join(self.keys())
+
+
+    def fromstr(self, line, parameters=()):
+        params = dict(parameters)
+        if self.satisfies(params, (self.keys(), line)):
+            for i, k in enumerate(self.keys()):
+                self[k] = line.split(self.FIELD_SEP)[i]
+        else:
+            raise UnmatchedModelException('Parameters not satified: ' +
+                                          str(params))
+
+
+    def __str__(self):
+        return self.FIELD_SEP.join(self.values()) + '\n'
+
+
+    @classmethod
+    def satisfies(cls, parameters, obj):
+        keys, line = obj
+        indices = dict([(k, i) for i, k in enumerate(keys)])
+        tests = [False] * len(parameters)
+        for i, (k, v) in enumerate(parameters.items()):
+            if line.split(cls.FIELD_SEP)[indices[k]] == v: #FIXME range testing?
+                tests[i] = True
+        return sum(tests)
+
+
+
+class JSONDataModel(DataModel):
+    '''
+    JSON-encoded storage-backed data. Represents one row of a file.
+    '''
+    FIELD_SEP = ' | '
+
+    def __init__(self, template=()):
+        super(JSONDataModel, self).__init__(template)
+
+
+    def header(self):
+        return self.FIELD_SEP.join(self.keys())
+
+
+    def fromstr(self, line, parameters=()):
+        params = dict(parameters)
+        if self.satisfies(params, (self.keys(), line)):
+            for i, k in enumerate(self.keys()):
+                self[k] = json.loads(line.split(self.FIELD_SEP)[i])
+        else:
+            raise UnmatchedModelException('Parameters not satified: ' +
+                                          str(params))
+
+
+    def __str__(self):
+        row = [json.dumps(self[k], default=json_handler) for k in self.keys()]
+        return self.FIELD_SEP.join(row) + '\n'
+
+
+    @classmethod
+    def satisfies(cls, parameters, obj):
+        keys, line = obj
+        indices = dict([(k, i) for i, k in enumerate(keys)])
+        tests = [False] * len(parameters)
+        for i, (k, v) in enumerate(parameters.items()):
+            if line.split(cls.FIELD_SEP)[indices[k]] == v: #FIXME range testing?
+                tests[i] = True
+        return sum(tests)
+
+
+
+####################
+
+class DataStore(object):
+    '''
+    Storage-backed data-model collection.
+
+    Extend for non-text files.
+    '''
+    def __init__(self, klass, name, directory, keys, ext='dat'):
+        self.klass = klass
+        self.name = name
+        self.path = os.path.join(directory, name + '.' + ext)
+        self.fields = keys
+        self.data_models = {}  # dict of (line-number, data-model-idx) tuples
+        self.last_idx = 0
+
+
+    def check_validity(self):
+        '''
+        Test if file contents correspond to the expected data structure.
+        '''
+        if not os.path.exists(self.path):
+            raise UnmatchedModelException(os.path.basename(self.path) +
+                                          ' does not exist.')
+        data_file = open(self.path, mode='r')
+        line = data_file.readline()
+        data_file.close()
+        if line == self.klass.header():
+            raise UnmatchedModelException(os.path.basename(self.path) +
+                                          ' is not a ' + self.klass.__name__ +
+                                          ' data model store.')
+
+    def check_consistency(self, data_models):
+        how_many = 0
+        for d1, d2 in combinations(data_models):
+            if not d1.header() == d2.header():
+                how_many += 1
+        if how_many:
+            raise UnmatchedModelException('Inconsistent data collection: ' +
+                                          str(how_many) +
+                                          ' do not match model signature.')
+        return self.klass.FIELD_SEP.join(self.fields)
+
+
+    def add(self, obj):
+        '''
+        Add-in an additional data-model.
+        '''
+        if isinstance(obj, dict):
+            template = dict(obj)
+            obj = self.klass(template)
+        if not isinstance(obj, self.klass) or obj.keys() != self.fields:
+            raise UnmatchedModelException('Attempting to add unmatched ' +
+                                          'data model to collection.')
+        self.last_idx += 1
+        self.data_models[self.last_idx] = obj
+
+
+    def load(self, parameters=()):
+        '''
+        Populate the data objects from file.
+        '''
+        params = dict(parameters)
+        self.check_validity()
+        data_file = open(self.path, mode='r')
+        for idx, line in enumerate(data_file):
+            try:
+                obj = self.klass(self.fields)
+                obj.fromstr(line, params)
+                self.data_models[idx] = obj
+                self.last_idx = idx
+            except UnmatchedModelException:
+                pass
+        data_file.close()
+
+
+    def unload(self):
+        '''
+        Store the data objects to file.
+        '''
+        header = self.check_consistency(self.data_models.values())
+        written = []
+        out_exists = os.path.exists(self.path)
+        if out_exists:
+            self.check_validity()
+            shutil.move(self.path, self.path + '.tmp')
+            data_in_file = open(self.path + '.tmp', 'r')
+        data_out_file = open(self.path, mode='a')
+        data_out_file.write(header + '\n')
+        if out_exists:
+            for idx, line in enumerate(data_in_file):
+                try:
+                    ## changed lines
+                    data_out_file.write(str(self.data_models[idx]) + '\n')
+                    written.append(idx)
+                except KeyError:
+                    ## lines not changed/ingested
+                    data_out_file.write(line + '\n')
+        for idx, data in self.data_models.items():
+            if not idx in written:
+                ## data that is new
+                data_out_file.write(str(data) + '\n')
+        if out_exists:
+            data_in_file.close()
+            os.remove(self.path + '.tmp')
+        data_out_file.close()
+
+
+
+try:
+    import h5py
+    import numpy
+
+    class HDF5DataModel(DataModel):
+        '''
+        HDF5 storage-backed data. Represents one row of a file.
+        '''
+        def __init__(self, template=()):
+            '''
+            Template can be a dictionary of dictionaries for implementing
+            multilevel attributes.
+            Basic type is an array.
+            '''
+            super(HDF5DataModel, self).__init__(template)
+
+
+        @classmethod
+        def satisfies(cls, parameters, obj):
+            tests = [True] * len(parameters)
+            for i, (k, v) in enumerate(parameters.items()):
+                for data in obj[k]:
+                    if data != v: #FIXME range testing?
+                        tests[i] = False
+            return sum(tests)
+
+
+
+    class HDF5DataStore(DataStore):
+        '''
+        HDF5 storage-backed data container.
+        Represents one specific group in the hierarchy of the file.
+        Description is a nested dictionary where leaves are types.
+        '''
+        def __init__(self, name, directory, description,
+                     hierarchy=(), dm=HDF5DataModel):
+            super(HDF5DataStore, self).__init__(dm, name, directory,
+                                            description, 'h5')
+            self.hierarchy = list(hierarchy)
+            self.data_models = []
+
+
+        def __recur(self, head, items, do_this, assign=False):
+            for k, v in items:
+                try:
+                    if isinstance(v, dict):
+                        self.__recur(head[k], v.items(), do_this, assign)
+                    elif assign:
+                        head[k] = do_this(head[k])
+                    else:
+                        do_this(head[k])
+                except KeyError:
+                    filename = os.path.basename(self.path)
+                    raise UnmatchedModelException(filename + ' is not a ' +
+                                                  self.klass.__name__ +
+                                                  ' data model store.')
+
+
+        def check_validity(self):
+            if not os.path.exists(self.path):
+                raise UnmatchedModelException(os.path.basename(self.path) +
+                                              ' does not exist.')
+            h5file = open(self.path, mode='r')
+            self.__recur(h5file, self.fields.items(), lambda: None)
+            h5file.close()
+
+
+        def add(self, obj):
+            '''
+            Add-in an additional data-model.
+            '''
+            if isinstance(obj, dict):
+                template = dict(obj)
+                self.__recur(template, template.items(),
+                             lambda x: None if isinstance(x, type) else x, True)
+                obj = self.klass(template)
+            if not isinstance(obj, self.klass) or obj.keys() != self.fields:
+                raise UnmatchedModelException('Attempting to add unmatched ' +
+                                              'data model to collection.')
+            self.data_models.append(obj)
+
+
+        def load(self, parameters=()):
+            params = dict(parameters)
+            self.check_validity()
+            h5file = h5py.File(self.path, mode='r')
+            node = h5file
+            for path in self.hierarchy:
+                for level in path.split('/'):
+                    if level:
+                        node = node[level]
+                for data in node:
+                    if self.klass.satisfies(params, data):
+                        template = dict(self.fields)
+                        ## set all leaves to None
+                        self.__recur(template, template.items(),
+                                     lambda: None, True)
+                        obj = self.klass(template)
+                        for k in obj.keys():
+                            obj[k] = numpy.array(data[k]) ##FIXME field type?
+                        self.data_models.append(obj)
+            h5file.close()
+
+
+        def unload(self):
+            h5file = h5py.File(self.path, mode='w')
+            node = h5file
+            for path in self.hierarchy:
+                for level in path.split('/'):
+                    if level:
+                        node = node[level]
+                for data in self.data_models:
+                    for k, v in data:
+                        node[k] = v
+            h5file.close()
+
+
+    
+
+    try:
+        from spacepy import datamodel
+
+        class SpaceDataModel(datamodel.SpaceData):
+            '''
+            HDF5 storage-backed spacepy datamodel. Represents one row of a file.
+            '''
+            @classmethod
+            def satisfies(cls, parameters, obj):
+                tests = [True] * len(parameters)
+                for i, (k, v) in enumerate(parameters.items()):
+                    for data in obj[k]:
+                        if data != v: #FIXME range testing?
+                            tests[i] = False
+                return sum(tests)
+
+
+        class SpaceDataStore(HDF5DataStore):
+            def __init__(self, name, directory, description,
+                         hierarchy=(), dm=SpaceDataModel):
+                super(SpaceDataStore, self).__init__(name, directory,
+                                                     description, hierarchy, dm)
+
+
+            ##FIXME
+
+
+    except ImportError:
+        pass
+
+except ImportError:
+    pass
 
 
 ##############################
@@ -243,8 +623,7 @@ class PlotSeries(DataViewer):
     '''
     Abstract plotting class, represents a single two or three axis data series.
     '''
-    def __init__(self, *args, **kwargs):
-        DataViewer.__init__(self, **kwargs)
+    def __init__(self, *args, **kwargs):  # pylint: disable=W0613
         self.name = 'Series'
         self.axes = kwargs.get('axes', 2)
         self.x_values = []
